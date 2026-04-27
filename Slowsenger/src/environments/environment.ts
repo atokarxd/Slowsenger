d92727021f4d87bd8c7c8f94e54ecf3ebe563ef1
István Tokár <h378482@stud.u-szeged.hu>
2026-04-27 18:08:04 +0200

    fix: SupabaseUrl fixing

--- Slowsenger/src/environments/environment.ts
+++ Slowsenger/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: false,
-    supabaseUrl: 'https://ainenjvhxtwgqfrjfyyy.supabase.com',
+    supabaseUrl: 'https://ainenjvhxtwgqfrjfyyy.supabase.co',
     supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFpbmVuanZoeHR3Z3FmcmpmeXl5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzU3NDQzMDIsImV4cCI6MjA5MTMyMDMwMn0.7Lj8cTVgEZyGtQgW3SGt7BM9Woow1xOfBBc0Hs4Ywkw',
     metaAppId: '2403302566775915',
     backendUrl: 'http://localhost:4000',
